89c63b35fb33b6ccea5e41f2a71e37b52b1c56fa
Jagdeep Kaur <118887947+Jagkar@users.noreply.github.com>
2024-10-15 23:32:50 +0530

    4 approaches of event binding

--- src/components/T14_BindingEventHandlers.js
+++ src/components/T14_BindingEventHandlers.js
@@ -9,6 +9,7 @@ class T14_BindingEventHandlers extends Component {
       this.state = {
          message:'Hello'
       }
+      this.clickHandler=this.clickHandler.bind(this);
     }
     // clickHandler(){
     //     // this.setState({
@@ -18,7 +19,9 @@ class T14_BindingEventHandlers extends Component {
     //     // this keyword inside event handler is undefined, so event binding is necessary in react class component
     //     console.log(this);
     // }
-    clickHandler(){
+
+    // clickHandler(){
+    clickHandler=()=>{
         this.setState({
             message:'GoodBye'
         })
@@ -29,6 +32,8 @@ class T14_BindingEventHandlers extends Component {
       <div>
         <div>{this.state.message}</div>
         <button onClick={this.clickHandler}>Click</button>
+        {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
+        {/* <button onClick={()=>this.clickHandler()}>Click</button> */}
       </div>
     )
   }
@@ -36,5 +41,20 @@ class T14_BindingEventHandlers extends Component {
 
 export default T14_BindingEventHandlers
 // Ways to bind:
-// 1. Binding in render method: onClick={this.clickHandler.bind(this)}
+// 1. Binding in render method:
+// onClick={this.clickHandler.bind(this)}
 // now this keyword refers to the event bind component instance
+// this is ok, but every update to the state will cause the component to rerender, this will generate a brand new event handler on every render, impact is not severe in small applications, but could be troublesome in large applications and components that contain nested children components
+
+// 2. arrow functions in render method:
+// onClick={()=>this.clickHandler()}
+// we are calling the event handler and returning the value, ok, but still perfrmance issues
+// use if no renderning of nested children components
+
+// 3. 
+// this.clickHandler=this.clickHandler.bind(this);
+// binding the event handler in the constructor as opposed to binding in the render method
+// becaus ethe binding happens once in the constructor, this is better than binding in the render method.
+
+// 4. arrow function as a class property
+// clickHandler=()=>{}
